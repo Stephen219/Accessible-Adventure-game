05e09dafae771b1d687335bb6c1116195fcd5da0
Stephen Kariuki <stephenkariuki838@gmail.com>
2024-12-28 21:16:20 +0000

    working but not clean. bare minimum

--- group_5_game/src/app/game/page.jsx
+++ group_5_game/src/app/game/page.jsx
@@ -1,13 +1,14 @@
 'use client';
 import React from 'react';
 import ExampleComponent from '/src/components/ExampleComponent';
+import Game from '/src/components/Game';
 
 export default function GamePage() {
   return (
     <div>
       <h1>Welcome to the Game Page</h1>
       {/* You can delete this ExampleComponent tag if you want */}
-      <ExampleComponent />
+      <Game></Game>
     </div>
   );
 }
---
+++ group_5_game/src/components/Scene2.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Scene2 = () => {
+    return (
+        <div>
+            <h2>Scene 2: Forest</h2>
+            <p>
+                You find yourself in a shadowy forest. The trees are tall and ancient, their branches 
+                forming a thick canopy overhead. The air smells of moss and earth. Somewhere in the 
+                distance, you hear the faint rustle of leaves.
+            </p>
+            <p>What will you do next? Say "go to Scene 1" to return to the crossroads or "go to Scene 3" to explore further.</p>
+        </div>
+    );
+};
+
+export default Scene2;
