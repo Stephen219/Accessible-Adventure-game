dde2bf37eb20c3cc9547a3445a2f863a848746a0
Zainab Almeshari <almeshariz@cardiff.ac.uk>
2025-01-01 00:38:36 +0000

    creating a .css for the inventory

--- group_5_game/src/components/Inventory.jsx
+++ group_5_game/src/components/Inventory.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
+import '@/components/css/Inventory.css';
 import { textToSpeechHandler } from '@/components/handlers/text_SpeechHandler';
 
 const Inventory = () => {
